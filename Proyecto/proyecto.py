4cd63cbd058f7caa741cc9a3917eb65bf04e3a63
mestradag <m.estradag@uniandes.edu.co>
2024-04-28 23:00:25 -0500

    Función obejtivo más inicialización de X

--- Proyecto/proyecto.py
+++ Proyecto/proyecto.py
@@ -5,15 +5,11 @@ Autores: Maria Alejandra Estrada - 202021060
          Ernesto Carlos Perez - 202112530
 """
 
-#Importaciones
+# Importaciones
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
 
-import sys
-import os
-
-
-#Creación modelo
+# Creación modelo
 model = ConcreteModel()
 
 # Conjuntos
@@ -26,28 +22,28 @@ model.D = Param(initialize=2)
 model.E = Param(initialize=1)
 model.A = Param(initialize=4)
 
-prioridades={
+prioridades = {
     ('p1', 'p1'): 5, ('p1', 'p2'): 2, ('p1', 'p3'): 0,
     ('p2', 'p1'): 3, ('p2', 'p2'): 2, ('p2', 'p3'): 0,
     ('p3', 'p1'): 3, ('p3', 'p2'): 5, ('p3', 'p3'): 0
 }
 model.P = Param(model.i, model.j, initialize=prioridades)
 
-doctores={
+doctores = {
     ('p1', 'p1'): 2, ('p1', 'p2'): 0, ('p1', 'p3'): 0,
     ('p2', 'p1'): 0, ('p2', 'p2'): 0, ('p2', 'p3'): 0,
     ('p3', 'p1'): 1, ('p3', 'p2'): 0, ('p3', 'p3'): 0
 } 
 model.numD = Param(model.i, model.j, initialize=doctores)
 
-enfemeras={
+enfermeras = {
     ('p1', 'p1'): 0, ('p1', 'p2'): 2, ('p1', 'p3'): 1,
     ('p2', 'p1'): 2, ('p2', 'p2'): 2, ('p2', 'p3'): 1,
     ('p3', 'p1'): 0, ('p3', 'p2'): 0, ('p3', 'p3'): 0
 }
-model.numE = Param(model.i, model.j, initialize=enfemeras)
+model.numE = Param(model.i, model.j, initialize=enfermeras)
 
-administradores={
+administradores = {
     ('p1', 'p1'): 0, ('p1', 'p2'): 0, ('p1', 'p3'): 0,
     ('p2', 'p1'): 0, ('p2', 'p2'): 0, ('p2', 'p3'): 0,
     ('p3', 'p1'): 0, ('p3', 'p2'): 1, ('p3', 'p3'): 1
@@ -56,10 +52,33 @@ model.numA = Param(model.i, model.j, initialize=administradores)
 
 # Variables de decisión
 model.X = Var(model.i, model.j, domain=Binary)  
-model.Y = Var(model.i, model.j, model.k, domain=NonNegativeIntegers) 
+model.Y = Var(model.i, model.j, model.k, domain=PositiveReals) 
+
+# Función objetivo
+model.obj = Objective(expr=sum(model.X[i, j] * model.P[i, j] for i in model.i for j in model.j))
+
+# Definir restricciones de inicialización para las variables X
+def init_X_rule(model, i, j):
+    return model.X[i, j] == 0
+
+# Añadir las restricciones de inicialización al modelo
+model.init_X = Constraint(model.i, model.j, rule=init_X_rule)
 
 # Solver
-SolverFactory('glpk').solve(model)
-model.display()
-print('######### SOLUCIÓN DEL MODELO #########\n')
-print('------------------------------------')
+results=SolverFactory('glpk').solve(model)
+
+# Verificar si la solución es válida
+if (results.solver.status == SolverStatus.ok) and (results.solver.termination_condition == TerminationCondition.optimal):
+    # Mostrar la solución
+    model.display()
+
+    # Calcular y mostrar el valor de la función objetivo
+    objective_value = model.obj()
+    print('\nObjective Value:', objective_value)
+
+    print('######### SOLUCIÓN DEL MODELO #########\n')
+    print('------------------------------------')
+else:
+    print("No se encontró una solución óptima.")
+
+
