dce6dd597c59251831ef7fa8404d87b30e787af7
Ernesto Perez <ec.perez@uniandes.edu.co>
2024-04-28 19:44:32 -0500

    Modelo base

--- Proyecto/proyecto.py
+++ Proyecto/proyecto.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Quiz 5 - Proyecto
 Autores: Maria Alejandra Estrada - 202021060
@@ -8,29 +9,57 @@ Autores: Maria Alejandra Estrada - 202021060
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
 
+import sys
+import os
+
+
 #Creación modelo
 model = ConcreteModel()
 
-# Conjuntos y parametros
-
-model.Z = Set(initialize=['urgencias', 'consultorios', 'cirugias', 'hospitalarios'])
-model.i = Set(initialize=model.Z)
-model.j = Set(initialize=model.Z)
+# Conjuntos
+model.i = Set(initialize=['p1', 'p2', 'p3'])
+model.j = Set(initialize=model.i)
 model.k = Set(initialize=['d', 'e', 'a'])
-model.D = Set()
-model.E = Set()
-model.A = Set()
 
-model.P = Param(model.i, model.j, initialize=0)  
-model.numD = Param(model.i, model.j, initialize=0)  
-model.numE = Param(model.i, model.j, initialize=0) 
-model.numA = Param(model.i, model.j, initialize=0) 
+# Parametros
+model.D = Param(initialize=2)
+model.E = Param(initialize=1)
+model.A = Param(initialize=4)
+
+prioridades={
+    ('p1', 'p1'): 5, ('p1', 'p2'): 2, ('p1', 'p3'): 0,
+    ('p2', 'p1'): 3, ('p2', 'p2'): 2, ('p2', 'p3'): 0,
+    ('p3', 'p1'): 3, ('p3', 'p2'): 5, ('p3', 'p3'): 0
+}
+model.P = Param(model.i, model.j, initialize=prioridades)
+
+doctores={
+    ('p1', 'p1'): 2, ('p1', 'p2'): 0, ('p1', 'p3'): 0,
+    ('p2', 'p1'): 0, ('p2', 'p2'): 0, ('p2', 'p3'): 0,
+    ('p3', 'p1'): 1, ('p3', 'p2'): 0, ('p3', 'p3'): 0
+} 
+model.numD = Param(model.i, model.j, initialize=doctores)
+
+enfemeras={
+    ('p1', 'p1'): 0, ('p1', 'p2'): 2, ('p1', 'p3'): 1,
+    ('p2', 'p1'): 2, ('p2', 'p2'): 2, ('p2', 'p3'): 1,
+    ('p3', 'p1'): 0, ('p3', 'p2'): 0, ('p3', 'p3'): 0
+}
+model.numE = Param(model.i, model.j, initialize=enfemeras)
+
+administradores={
+    ('p1', 'p1'): 0, ('p1', 'p2'): 0, ('p1', 'p3'): 0,
+    ('p2', 'p1'): 0, ('p2', 'p2'): 0, ('p2', 'p3'): 0,
+    ('p3', 'p1'): 0, ('p3', 'p2'): 1, ('p3', 'p3'): 1
+}
+model.numA = Param(model.i, model.j, initialize=administradores) 
 
 # Variables de decisión
 model.X = Var(model.i, model.j, domain=Binary)  
-model.A = Var(model.i, model.j, model.k, domain=NonNegativeIntegers) 
+model.Y = Var(model.i, model.j, model.k, domain=NonNegativeIntegers) 
 
-#SolverFactory('mindtpy').solve(Model, mip_solver='glpk', nlp_solver='ipopt')
-#Model.display()
+# Solver
+SolverFactory('glpk').solve(model)
+model.display()
 print('######### SOLUCIÓN DEL MODELO #########\n')
 print('------------------------------------')
