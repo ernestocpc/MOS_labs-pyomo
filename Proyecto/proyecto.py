032d38bdcd56b2fcf38bddf49cbe9929e97b81ff
Ernesto P <ec.perez@uniandes.edu.co>
2024-04-30 14:16:15 -0500

    Arreglos proyecto

--- Proyecto/proyecto.py
+++ Proyecto/proyecto.py
@@ -13,8 +13,7 @@ from pyomo.opt import SolverFactory
 model = ConcreteModel()
 
 # Conjuntos
-model.i = Set(initialize=['p1', 'p2', 'p3'])
-model.j = Set(initialize=model.i)
+model.z = Set(initialize=['p1', 'p2', 'p3'])
 model.k = Set(initialize=['d', 'e', 'a'])
 
 # Parametros
@@ -27,58 +26,55 @@ prioridades = {
     ('p2', 'p1'): 3, ('p2', 'p2'): 2, ('p2', 'p3'): 0,
     ('p3', 'p1'): 3, ('p3', 'p2'): 5, ('p3', 'p3'): 0
 }
-model.P = Param(model.i, model.j, initialize=prioridades)
+model.P = Param(model.z, model.z, initialize=prioridades)
 
 doctores = {
     ('p1', 'p1'): 2, ('p1', 'p2'): 0, ('p1', 'p3'): 0,
     ('p2', 'p1'): 0, ('p2', 'p2'): 0, ('p2', 'p3'): 0,
     ('p3', 'p1'): 1, ('p3', 'p2'): 0, ('p3', 'p3'): 0
 } 
-model.numD = Param(model.i, model.j, initialize=doctores)
+model.numD = Param(model.z, model.z, initialize=doctores)
 
 enfermeras = {
     ('p1', 'p1'): 0, ('p1', 'p2'): 2, ('p1', 'p3'): 1,
     ('p2', 'p1'): 2, ('p2', 'p2'): 2, ('p2', 'p3'): 1,
     ('p3', 'p1'): 0, ('p3', 'p2'): 0, ('p3', 'p3'): 0
 }
-model.numE = Param(model.i, model.j, initialize=enfermeras)
+model.numE = Param(model.z, model.z, initialize=enfermeras)
 
 administradores = {
     ('p1', 'p1'): 0, ('p1', 'p2'): 0, ('p1', 'p3'): 0,
     ('p2', 'p1'): 0, ('p2', 'p2'): 0, ('p2', 'p3'): 0,
     ('p3', 'p1'): 0, ('p3', 'p2'): 1, ('p3', 'p3'): 1
 }
-model.numA = Param(model.i, model.j, initialize=administradores) 
+model.numA = Param(model.z, model.z, initialize=administradores) 
 
 # Variables de decisión
-model.X = Var(model.i, model.j, domain=Binary)  
-model.Y = Var(model.i, model.j, model.k, domain=PositiveReals) 
+model.X = Var(model.z, model.z, domain=Binary)  
+model.Y = Var(model.z, model.z, model.k, domain=PositiveReals) 
 
 # Función objetivo
-model.multi_objective = Objective(expr=sum(model.X[i, j] * model.P[i, j] for i in model.i for j in model.j), sense=maximize)
+model.multi_objective = Objective(expr=sum(model.X[i, j] * model.P[i, j] for i in model.z for j in model.z), sense=maximize)
 
-# Definir restricciones de inicialización para las variables X
-def init_X_rule(model, i, j):
-    return model.X[i, j] == 0
 
-# Añadir las restricciones de inicialización al modelo
-model.init_X = Constraint(model.i, model.j, rule=init_X_rule)
+"""
+Restriccion 2: No se pueden asignar m ́as doctores a una zona que el total
+de doctores que est ́an disponibles para el hospital, indicados en los par ́ametros
+iniciales.
+
+"""
+def restriccion2_rule(model, k):
+    if k == 'd':
+        return sum(model.Y[i, j, k] for i in model.z for j in model.z) <= model.D
+    else:
+        return Constraint.Skip
+model.restriccion2 = Constraint(model.k, rule=restriccion2_rule)
+
+
 
 # Solver
 results=SolverFactory('glpk').solve(model)
+model.display()
 
-# Verificar si la solución es válida
-if (results.solver.status == SolverStatus.ok) and (results.solver.termination_condition == TerminationCondition.optimal):
-    # Mostrar la solución
-    model.display()
-
-    # Calcular y mostrar el valor de la función objetivo
-    objective_value = model.obj()
-    print('\nObjective Value:', objective_value)
-
-    print('######### SOLUCIÓN DEL MODELO #########\n')
-    print('------------------------------------')
-else:
-    print("No se encontró una solución óptima.")
 
 
