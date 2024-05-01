723efa35d6c60cadd6b0f8801f2279d602d24b81
mestradag <m.estradag@uniandes.edu.co>
2024-05-01 11:48:02 -0500

    Corregido

--- Proyecto/proyecto.py
+++ Proyecto/proyecto.py
@@ -22,30 +22,30 @@ model.E = Param(initialize=1)
 model.A = Param(initialize=4)
 
 prioridades = {
-    ('p1', 'p1'): 5, ('p1', 'p2'): 2, ('p1', 'p3'): 0,
-    ('p2', 'p1'): 3, ('p2', 'p2'): 2, ('p2', 'p3'): 0,
-    ('p3', 'p1'): 3, ('p3', 'p2'): 5, ('p3', 'p3'): 0
+    ('p1', 'p1'): 5, ('p1', 'p2'): 2, ('p1', 'p3'): 1,
+    ('p2', 'p1'): 3, ('p2', 'p2'): 2, ('p2', 'p3'): 1,
+    ('p3', 'p1'): 3, ('p3', 'p2'): 5, ('p3', 'p3'): 2
 }
 model.P = Param(model.z, model.z, initialize=prioridades)
 
 doctores = {
-    ('p1', 'p1'): 2, ('p1', 'p2'): 0, ('p1', 'p3'): 0,
-    ('p2', 'p1'): 0, ('p2', 'p2'): 0, ('p2', 'p3'): 0,
-    ('p3', 'p1'): 1, ('p3', 'p2'): 0, ('p3', 'p3'): 0
+    ('p1', 'p1'): 0.1, ('p1', 'p2'): 0.1, ('p1', 'p3'): 0.1,
+    ('p2', 'p1'): 0.1, ('p2', 'p2'): 0.1, ('p2', 'p3'): 0.1,
+    ('p3', 'p1'): 0.1, ('p3', 'p2'): 0.1, ('p3', 'p3'): 0.1
 } 
 model.numD = Param(model.z, model.z, initialize=doctores)
 
 enfermeras = {
-    ('p1', 'p1'): 0, ('p1', 'p2'): 2, ('p1', 'p3'): 1,
-    ('p2', 'p1'): 2, ('p2', 'p2'): 2, ('p2', 'p3'): 1,
-    ('p3', 'p1'): 0, ('p3', 'p2'): 0, ('p3', 'p3'): 0
+    ('p1', 'p1'): 0.1, ('p1', 'p2'): 0.1, ('p1', 'p3'): 0.1,
+    ('p2', 'p1'): 0.1, ('p2', 'p2'): 0.1, ('p2', 'p3'): 0.1,
+    ('p3', 'p1'): 0.1, ('p3', 'p2'): 0.1, ('p3', 'p3'): 0.1
 }
 model.numE = Param(model.z, model.z, initialize=enfermeras)
 
 administradores = {
-    ('p1', 'p1'): 0, ('p1', 'p2'): 0, ('p1', 'p3'): 0,
-    ('p2', 'p1'): 0, ('p2', 'p2'): 0, ('p2', 'p3'): 0,
-    ('p3', 'p1'): 0, ('p3', 'p2'): 1, ('p3', 'p3'): 1
+    ('p1', 'p1'): 0.1, ('p1', 'p2'): 0.1, ('p1', 'p3'): 0.1,
+    ('p2', 'p1'): 0.1, ('p2', 'p2'): 0.1, ('p2', 'p3'): 0.1,
+    ('p3', 'p1'): 0.1, ('p3', 'p2'): 0.1, ('p3', 'p3'): 0.1
 }
 model.numA = Param(model.z, model.z, initialize=administradores) 
 
@@ -54,12 +54,17 @@ model.X = Var(model.z, model.z, domain=Binary)
 model.Y = Var(model.z, model.z, model.k, domain=PositiveReals) 
 
 # Función objetivo
+"""
+La Función Objetivo busca maximizar las zonas cubiertas seg ́un su prioridad
+por el personal necesario. Dado que se usa un sistema de prioridad donde 1 es
+minimo y 5 es maximo el algoritmo va a priorizar cubrir zonas con alta prioridad.
+"""
 model.multi_objective = Objective(expr=sum(model.X[i, j] * model.P[i, j] for i in model.z for j in model.z), sense=maximize)
 
-
+#Restricción
 """
-Restriccion 2: No se pueden asignar m ́as doctores a una zona que el total
-de doctores que est ́an disponibles para el hospital, indicados en los par ́ametros
+Restricción 2: No se pueden asignar más doctores a una zona que el total
+de doctores que están disponibles para el hospital, indicados en los parámetros
 iniciales.
 
 """
@@ -73,8 +78,6 @@ model.restriccion2 = Constraint(model.k, rule=restriccion2_rule)
 
 
 # Solver
-results=SolverFactory('glpk').solve(model)
+results = SolverFactory('glpk').solve(model)
 model.display()
-
-
-
+print("\nFuncion objetivo:", model.multi_objective())
