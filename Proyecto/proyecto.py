6dce593711f7f07171ce9fabf2eb08bc38ab867a
Ernesto Perez <ec.perez@uniandes.edu.co>
2024-05-17 19:58:42 -0500

    Ajustes proyecto

--- Proyecto/proyecto.py
+++ Proyecto/proyecto.py
@@ -49,7 +49,6 @@ administradores = {
 model.numA = Param(model.z, model.z, initialize=administradores) 
 
 # Variables de decisión
-model.X = Var(model.z, model.z, domain=Binary)  
 model.Y = Var(model.z, model.z, model.k, domain=PositiveReals) 
 
 # Función objetivo
@@ -58,56 +57,47 @@ model.multi_objective = Objective(expr=sum(model.Y[i, j, k] * model.P[i, j] for 
 # Restricción 1: No se pueden asignar más doctores a una zona que el total
 # de doctores que están disponibles para el hospital, indicados en los parámetros
 # iniciales.
-def restriccion2_rule(model, k):
+def restriccion1_rule(model, k):
     if k == 'd':
         return sum(model.Y[i, j, k] for i in model.z for j in model.z) <= model.D
     else:
         return Constraint.Skip
-model.restriccion2 = Constraint(model.k, rule=restriccion2_rule)
+model.restriccion1 = Constraint(model.k, rule=restriccion1_rule)
 
 # Restricción 2: No se pueden asignar más enfermeras a una zona que el total
 # de enfermeras que están disponibles para el hospital, indicados en los parámetros
 # iniciales.
-def restriccion3_rule(model, k):
+def restriccion2_rule(model, k):
     if k == 'e':
         return sum(model.Y[i, j, k] for i in model.z for j in model.z) <= model.E
     else:
         return Constraint.Skip
-model.restriccion3 = Constraint(model.k, rule=restriccion3_rule)
+model.restriccion2 = Constraint(model.k, rule=restriccion2_rule)
 
 # Restricción 3: No se pueden asignar más administradores a una zona que el total
 # de doctores que están disponibles para el hospital, indicados en los parámetros
 # iniciales.
-def restriccion4_rule(model, k):
+def restriccion3_rule(model, k):
     if k == 'a':
         return sum(model.Y[i, j, k] for i in model.z for j in model.z) <= model.A
     else:
         return Constraint.Skip
-model.restriccion4 = Constraint(model.k, rule=restriccion4_rule)
+model.restriccion3 = Constraint(model.k, rule=restriccion3_rule)
 
-# Restricción 4: Relacionar variable X y Y 
+# Restricción 4: Solo se pueden asignar doctores donde se requieran
+def restriccion4_rule(model, i, j):
+    return model.Y[i, j, 'd'] <= model.numD[i, j]
+model.restriccion4 = Constraint(model.z, model.z, rule=restriccion4_rule)
+
+# Restricción 5: Solo se pueden asignar enfermeras donde se requieran
 def restriccion5_rule(model, i, j):
-   return sum(model.Y[i, j, k] for k in model.k) <= model.X[i, j] * 100000000
-
+    return model.Y[i, j, 'e'] <= model.numE[i, j]
 model.restriccion5 = Constraint(model.z, model.z, rule=restriccion5_rule)
 
-# Restricción 5: Solo se pueden asignar doctores donde se requieran
+# Restricción 6: Solo se pueden asignar administradores donde se requieran
 def restriccion6_rule(model, i, j):
-    return model.Y[i, j, 'd'] <= model.numD[i, j]
-
-model.restriccion6 = Constraint(model.z, model.z, rule=restriccion6_rule)
-
-# Restricción 6: Solo se pueden asignar enfermeras donde se requieran
-def restriccion7_rule(model, i, j):
-    return model.Y[i, j, 'e'] <= model.numE[i, j]
-
-model.restriccion7 = Constraint(model.z, model.z, rule=restriccion7_rule)
-
-# Restricción 7: Solo se pueden asignar administradores donde se requieran
-def restriccion8_rule(model, i, j):
     return model.Y[i, j, 'a'] <= model.numA[i, j]
-
-model.restriccion8 = Constraint(model.z, model.z, rule=restriccion8_rule)
+model.restriccion6 = Constraint(model.z, model.z, rule=restriccion6_rule)
 
 # Solver
 SolverFactory('glpk').solve(model)
